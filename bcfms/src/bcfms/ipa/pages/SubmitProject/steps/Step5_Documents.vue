97472ede43b9b2b8b16529cb1f32e5ca8da8bf60
Yevgeny <evgenyg@live.com>
2025-07-10 10:58:41 -0600

    54 Use component lab FileListWidget for Step5

--- bcfms/src/bcfms/ipa/pages/SubmitProject/steps/Step5_Documents.vue
+++ bcfms/src/bcfms/ipa/pages/SubmitProject/steps/Step5_Documents.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { useTemplateRef } from 'vue';
 import type { Ref } from 'vue';
+import FileListWidget from '@/arches_component_lab/widgets/FileListWidget/FileListWidget.vue';
+import { EDIT } from '@/arches_component_lab/widgets/constants.ts';
 
 import { Form, type FormInstance } from '@primevue/forms';
 //import type { IPA } from '@/bcfms/ipa/schema/IPASchema.ts';
@@ -22,14 +24,29 @@ defineExpose({ isValid });
         name="projectDocumentsForm"
         :validateOnBlur="true"
     >
-        Upload project documents such as Application Information Requirements
-        (AIR) or Land Act project description document, if applicable
+        <p class="p-font-bold">
+            Upload project documents such as Application Information
+            Requirements (AIR) or Land Act project description document, if
+            applicable
+        </p>
+        <div class="div-file-widget">
+            <FileListWidget
+                :mode="EDIT"
+                :initial-value="null"
+                graph-slug="project_assessment"
+                node-alias="project_documents"
+                :show-label="false"
+            />
+        </div>
     </Form>
 </template>
 
-<style>
-.inline-block {
-    display: inline-block;
-    width: unset;
+<style scoped>
+.p-font-bold {
+    font-weight: bold;
+}
+.div-file-widget {
+    margin-top: 2rem;
+    margin-left: 20rem;
 }
 </style>
