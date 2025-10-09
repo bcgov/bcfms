e8b79566defb4630d827adcd66de9f545043a8b8
Yevgeny <evgenyg@live.com>
2025-10-09 12:06:38 -0600

    92 Replace old Editor widget with GenericWidget

--- bcfms/src/bcfms/ipa/pages/ReviewProject/steps/Step4_RiskAssessment.vue
+++ bcfms/src/bcfms/ipa/pages/ReviewProject/steps/Step4_RiskAssessment.vue
@@ -3,7 +3,6 @@ import { useTemplateRef, inject } from 'vue';
 import type { Ref } from 'vue';
 
 import LabelledInput from '@/bcgov_arches_common/components/labelledinput/LabelledInput.vue';
-import Editor from 'primevue/editor';
 import { Form, type FormInstance } from '@primevue/forms';
 import { zodResolver } from '@primevue/forms/resolvers/zod';
 import type { IPA } from '@/bcfms/ipa/schema/IPASchema.ts';
@@ -103,14 +102,14 @@ defineExpose({ isValid });
             input-name="initialReviewInternalNotes"
             :required="true"
         >
-            <Editor
-                id="initialReviewInternalNotes"
-                ref="initialReviewInternalNotesField"
-                v-model="ipa.initialProjectReview.initial_review_internal_notes"
-                theme="snow"
-                aria-describedby="initial-review-internal-notes-help"
-                aria-required="true"
-                fluid
+            <GenericWidget
+                :mode="EDIT"
+                :aliased-node-data="
+                    ipa.initialProjectReview?.initial_review_internal_notes
+                "
+                placeholder="Enter Initial Review Internal Notes"
+                graph-slug="project_assessment"
+                node-alias="initial_review_internal_notes"
                 @update:value="
                     updateModelValue($event, 'initial_review_internal_notes')
                 "
