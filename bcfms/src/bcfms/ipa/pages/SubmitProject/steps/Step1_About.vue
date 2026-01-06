be26f0da420093ea374238e635d2ce2726c3b8b8
brett <brett@qedsystems.ca>
2026-01-05 16:25:47 -0800

    Update step 2 title and add Step1 requirements text

--- bcfms/src/bcfms/ipa/pages/SubmitProject/steps/Step1_About.vue
+++ bcfms/src/bcfms/ipa/pages/SubmitProject/steps/Step1_About.vue
@@ -12,9 +12,20 @@ defineExpose({ isValid });
         <p>The entire submission will need to be completed at once.</p>
         <p>You will need the following:</p>
         <ol class="ol-margin-left">
-            <li>First thing</li>
-            <li>Second thing</li>
-            <li>Third thing</li>
+            <li>
+                Project referral information giving the project name, contact
+                information for the Industry initiator (individual and/or
+                company), Authorizing Agency, if applicable and estimated start
+                date (if known)
+            </li>
+            <li>Details about the project type and its proposed activity</li>
+            <li>
+                A brief description of the project location and associated KML
+                or shapefile files
+            </li>
+            <li>
+                Project documents (EAO AIR or Land Act documents), if applicable
+            </li>
         </ol>
         <p>
             When you are ready to proceed, press the 'Start' button to start the
