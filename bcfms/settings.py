001f0a9206bc501ea168551c106b09ed4857fd99
brett <brett@qedsystems.ca>
2026-03-24 11:16:01 -0700

    Add GH automation and deployment context

--- bcfms/settings.py
+++ bcfms/settings.py
@@ -38,6 +38,7 @@ load_dotenv(
 APP_NAME = "bcfms"
 APP_VERSION = semantic_version.Version(major=1, minor=2, patch=2)
 APP_ROOT = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+DEPLOYMENT_TIMESTAMP=get_env_variable("DEPLOYMENT_TIMESTAMP")
 
 # PROXY prefix used - NB - cannot have leading "/", and must have trailing "/"
 BCGOV_PROXY_PREFIX = get_env_variable("BCGOV_PROXY_PREFIX")
@@ -280,6 +281,10 @@ STATICFILES_DIRS = build_staticfiles_dirs(app_root=APP_ROOT)
 TEMPLATES = build_templates_config(
     debug=DEBUG,
     app_root=APP_ROOT,
+)
+
+TEMPLATES[0]['OPTIONS']['context_processors'] += (
+    'bcfms.util.context_processors.deployment_settings',
 )
 
 ALLOWED_HOSTS = get_env_variable("ALLOWED_HOSTS").split()
