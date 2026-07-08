fc2b39839f6986dd4ca3a5f80ba9aca5a7554d42
brett <brett@qedsystems.ca>
2026-07-08 15:07:33 -0700

    202 - Fix search string for IPA paleo org & lead 210 - Fix config and data for contributor inactive flag

--- bcfms/migrations/0202_fix_paleo_search_string.py
+++ bcfms/migrations/0202_fix_paleo_search_string.py
@@ -2,7 +2,8 @@ from django.db import migrations
 from arches.app.models import models
 
 update_notes = "202 - Fixed paleo consultant and paleo company search strings"
-correct_company_search_string = '/bc-fossil-management/search?advanced-search=[{"op"%3A"and"%2C"c4eca2de-9dea-11ed-9a7b-5254004d77d3"%3A{"op"%3A"eq"%2C"val"%3A"a7a00d7b-ac19-4b7a-b911-55b4fb1473ff"}%2C"e2f1c29a-c500-11ed-9089-5254004d77d3"%3A{"op"%3A"eq"%2C"val"%3A"3489e41e-0168-4a8f-80f1-75fde26e2130"}%2C"c4ec9b2c-9dea-11ed-9a7b-5254004d77d3"%3A{"op"%3A"~"%2C"lang"%3A"en"%2C"val"%3A""}%2C"c4eca13a-9dea-11ed-9a7b-5254004d77d3"%3A{"op"%3A"~"%2C"lang"%3A"en"%2C"val"%3A""}%2C"a5b2cd72-c4f4-11ed-a06d-5254004d77d3"%3A{"op"%3A"~"%2C"lang"%3A"en"%2C"val"%3A""}%2C"b5ed0072-c4f4-11ed-94de-5254004d77d3"%3A{"op"%3A"~"%2C"lang"%3A"en"%2C"val"%3A""}%2C"009ba8da-faf6-11ed-ac37-5254004d77d3"%3A{"op"%3A""%2C"val"%3A""}%2C"99643f1e-9e4e-11ed-b6d5-5254004d77d3"%3A{"val"%3A"null"}}]&sort-results=asc&sort-by=resource_name&sort-order=asc'
+correct_company_search_string = '/bc-fossil-management/search?advanced-search=[{"op"%3A"and"%2C"c4eca2de-9dea-11ed-9a7b-5254004d77d3"%3A{"op"%3A"eq"%2C"val"%3A"a7a00d7b-ac19-4b7a-b911-55b4fb1473ff"}%2C"e2f1c29a-c500-11ed-9089-5254004d77d3"%3A{"op"%3A"eq"%2C"val"%3A"3489e41e-0168-4a8f-80f1-75fde26e2130"}%2C"c4ec9b2c-9dea-11ed-9a7b-5254004d77d3"%3A{"op"%3A"~"%2C"lang"%3A"en"%2C"val"%3A""}%2C"c4eca13a-9dea-11ed-9a7b-5254004d77d3"%3A{"op"%3A"~"%2C"lang"%3A"en"%2C"val"%3A""}%2C"a5b2cd72-c4f4-11ed-a06d-5254004d77d3"%3A{"op"%3A"~"%2C"lang"%3A"en"%2C"val"%3A""}%2C"b5ed0072-c4f4-11ed-94de-5254004d77d3"%3A{"op"%3A"~"%2C"lang"%3A"en"%2C"val"%3A""}%2C"009ba8da-faf6-11ed-ac37-5254004d77d3"%3A{"op"%3A""%2C"val"%3A""}%2C"99643f1e-9e4e-11ed-b6d5-5254004d77d3"%3A{"val"%3A"f"}}]&sort-results=asc&sort-by=resource_name&sort-order=asc'
+correct_lead_paleo_search_string = '/bc-fossil-management/search?advanced-search=[{"op"%3A"and"%2C"c4eca2de-9dea-11ed-9a7b-5254004d77d3"%3A{"op"%3A"eq"%2C"val"%3A"706881ba-bda3-4fbc-9d66-c26bf2d92c18"}%2C"e2f1c29a-c500-11ed-9089-5254004d77d3"%3A{"op"%3A"eq"%2C"val"%3A"3489e41e-0168-4a8f-80f1-75fde26e2130"}%2C"c4ec9b2c-9dea-11ed-9a7b-5254004d77d3"%3A{"op"%3A"~"%2C"lang"%3A"en"%2C"val"%3A""}%2C"c4eca13a-9dea-11ed-9a7b-5254004d77d3"%3A{"op"%3A"~"%2C"lang"%3A"en"%2C"val"%3A""}%2C"a5b2cd72-c4f4-11ed-a06d-5254004d77d3"%3A{"op"%3A"~"%2C"lang"%3A"en"%2C"val"%3A""}%2C"b5ed0072-c4f4-11ed-94de-5254004d77d3"%3A{"op"%3A"~"%2C"lang"%3A"en"%2C"val"%3A""}%2C"009ba8da-faf6-11ed-ac37-5254004d77d3"%3A{"op"%3A""%2C"val"%3A""}%2C"99643f1e-9e4e-11ed-b6d5-5254004d77d3"%3A{"val"%3A"f"}}]&sort-results=asc&sort-by=resource_name&sort-order=asc'
 
 
 def fix_paleo_search_string(apps, schema_editor):
@@ -20,10 +21,7 @@ def fix_paleo_search_string(apps, schema_editor):
         alias="paleo_consultant",
         is_immutable=False,
     )
-    stripped_search_string = node_to_update.config["searchString"].replace(
-        "http://localhost:81", ""
-    )
-    node_to_update.config["searchString"] = stripped_search_string
+    node_to_update.config["searchString"] = correct_lead_paleo_search_string
     node_to_update.save()
 
     # Fix the paleo company search string
